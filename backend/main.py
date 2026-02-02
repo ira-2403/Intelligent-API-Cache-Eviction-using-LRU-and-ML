5666abb65f6da19a2261989de0313b43f8c1239a
samriddhi <samriddhishaw123@gmail.com>
2026-02-02 23:50:49 +0530

    Corrected bugs for FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from collections.abc import Set
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import requests
@@ -5,28 +6,30 @@ import time
 from backend.cache.lru_cache import LRUCache
 app=FastAPI()
 cache=LRUCache(capacity=5)
-class APIRequest(BaseModel):
-    url:set[str]
+class RequestModel(BaseModel):
+    url:Set[str]
 @app.post("/request")
-def handle_request(request: APIRequest):
+def handle_request(request:RequestModel):
     start_time=time.time()
-    cached_response=cache.get(request.url)
+    urls=list(request.url)
+    url=urls[0]
+    cached_response=cache.get(url)
     if cached_response:
         response_time=int((time.time()-start_time)*1000)
-        return{
+        return {
             "data":cached_response,
-            "cach_status":"HIT",
+            "cache_status":"HIT",
             "response_time_ms":response_time
         }
     try:
-        api_response=requests.get(requests.url)
+        api_response=requests.get(url)
         api_response.raise_for_status()
         data=api_response.json()
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    cache.put(request.url,data)
+        raise HTTPException(status_code=400,detail=str(e))
+    cache.put(url,data)
     response_time=int((time.time()-start_time)*1000)
-    return{
+    return {
         "data":data,
         "cache_status":"MISS",
         "response_time_ms":response_time
